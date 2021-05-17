32f132b1712128e7e5c6e925e386ab964106a435
Rockie Guo <rockie@kitmi.com.au>
2021-05-17 18:25:41 +1000

    Add HttpCode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const AppErrors = require('./AppErrors');
 const DataErrors = require('./DataErrors');
 const HttpErrors = require('./HttpErrors');
+const HttpCode = require('http-status-codes');
 
-module.exports = { ...AppErrors, ...DataErrors, ...HttpErrors };
+module.exports = { ...AppErrors, ...DataErrors, ...HttpErrors, HttpCode };
